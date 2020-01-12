f614d95d10a453be2fc30ef283a4ce727535d18a
Qstick <qstick@gmail.com>
2020-01-11 22:13:28 -0500

    Fixed: Remove Static/Dynamic Settings, Allow Folder Move from Editor

---
+++ src/NzbDrone.Core/Datastore/Migration/167_remove_movie_pathstate.cs
@@ -0,0 +1,16 @@
+using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(167)]
+    public class remove_movie_pathstate : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Delete.Column("PathState").FromTable("Movies");
+
+            Execute.Sql("DELETE FROM Config WHERE [KEY] IN ('pathsdefaultstatic')");
+        }
+    }
+}
